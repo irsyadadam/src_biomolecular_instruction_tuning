08a509af7a983807625ecafcb241fa808d400fe9
hi im irsyad <irsyadadam@g.ucla.edu>
2025-10-28 00:25:44 -0700

    final changes

--- DEEPSPEED_finetune_graph_proteomics.sh
+++ DEEPSPEED_finetune_graph_proteomics.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # DEEPSPEED_finetune_graph_proteomics.sh
 
-export PYTHONPATH="${PYTHONPATH}:/home/src_biomolecule_instruction_tuning"
+export PYTHONPATH="${PYTHONPATH}:/home/irsyadadam/src_biomolecule_instruction_tuning"
 
-CUDA_GPU="0,1"
+CUDA_GPU="0"
 
-DATA_PATH="/path/to/data/proteomics_finetune_conversations.json"
-PATIENT_GRAPHS_DIR="/path/to/patient_graphs"
-PRETRAINED_MODEL_PATH="/path/to/graph_llm/pretrain"
-OUTPUT_DIR="/path/to/graph_llm/finetune"
+DATA_PATH="/local/irsyadadam/biomolecular_instruction_tuning_data/final_data/proteomics_finetune_conversations.json"
+PATIENT_GRAPHS_DIR="../biomolecule_instruction_tuning/data/patient_graphs"
+PRETRAINED_MODEL_PATH="/local/irsyadadam/biomolecular_instruction_tuning_data/graph_llm/pretrain"
+OUTPUT_DIR="/local/irsyadadam/biomolecular_instruction_tuning_data/graph_llm/finetune"
 
 LLM_VERSION="lmsys/vicuna-7b-v1.5"
 VISION_TOWER="graph_tower"
--- DEEPSPEED_pretrain_graph_proteomics.sh
+++ DEEPSPEED_pretrain_graph_proteomics.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-export PYTHONPATH="${PYTHONPATH}:/home/src_biomolecule_instruction_tuning"
+export PYTHONPATH="${PYTHONPATH}:/home/irsyadadam/src_biomolecule_instruction_tuning"
 
 # GPU Configuration
-CUDA_GPU="0,1"
+CUDA_GPU="0"
 
 # Data Paths
-DATA_PATH="/path/to/data/proteomics_finetune_conversations.json"
-PATIENT_GRAPHS_DIR="/path/to/patient_graphs"
-OUTPUT_DIR="/path/to/graph_llm/pretrain"
-
+DATA_PATH="/local/irsyadadam/biomolecular_instruction_tuning_data/final_data/proteomics_pretrain_conversations.json"
+PATIENT_GRAPHS_DIR="../biomolecule_instruction_tuning/data/patient_graphs"  # Update this path
+OUTPUT_DIR="/local/irsyadadam/biomolecular_instruction_tuning_data/graph_llm/pretrain"
 
 # Model Configuration
 LLM_VERSION="lmsys/vicuna-7b-v1.5"
